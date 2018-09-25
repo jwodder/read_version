c7f943d4b6c501db5983464bbe296a68e176cec3
John T. Wodder II <git@varonathe.org>
2018-09-25 16:05:47 +0000

    Packaging boilerplate

--- read_version.py
+++ read_version.py
@@ -1,3 +1,15 @@
+"""
+Extract your project's __version__ variable
+
+Visit <https://github.com/jwodder/read_version> for more information.
+"""
+
+__version__      = '0.1.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'read-version@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/read_version'
+
 import ast
 import inspect
 import os.path
---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools   import setup
+from read_version import read_version
+
+setup(version=read_version('read_version.py'))
