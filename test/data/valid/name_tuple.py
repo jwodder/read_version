3ab3b3c1a580abbcc8a8e93e774a417096dae478
John T. Wodder II <git@varonathe.org>
2019-04-28 18:42:53 +0000

    Tests of some corner cases

---
+++ test/data/valid/name_tuple.py
@@ -0,0 +1,2 @@
+__version__ = (__version__, a) = ('1.2.3', 23)
+__custom__ = (__custom__, b) = (42, '3.4.5')
---
+++ test/data/valid/tuple_name.py
@@ -0,0 +1,2 @@
+(__version__, a, b, c, d) = __version__ = '1.2.3'
+(__custom__, x) = (x, __custom__) = (23, 42)
